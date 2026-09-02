43fb7ef53ddfc9278950b9a681ef3dc704078718
coconut3082010-droid <coconut3082010@gmail.com>
2026-09-02 20:17:00 +0700

    Change loading title in testing script

--- Cheating during testing.lua
+++ Cheating during testing.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Coconut Hub",
-    LoadingTitle = "Coconut Hub",
+    LoadingTitle = "cheating during testing script",
     LoadingSubtitle = "by Coconut",
     Theme = "Default",
     DisableRayfieldPrompts = false,
